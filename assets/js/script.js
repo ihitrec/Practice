cdebabe21958e7a48cdde6e4d754a003350a7a62
ihitrec <hitrec555@gmail.com>
2021-03-05 22:54:34 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,13 @@ function moveFoward() {
     let firstImg = cellArray[0];
     let lastImg = cellArray[5];
     let lastImgOrder = parseInt($(lastImg).css("order"));
-    let moveValue = parseInt($(".images").css("left"));
-    $(".images").animate({ left: `${moveValue - 320}px` }, 900);
+    let moveValue = parseInt($(".images").css("margin-left"));
+    $(".images").animate({ marginLeft: `${moveValue - 320}px` }, 900);
     $(".foward").unbind("click");
     $(".backward").unbind("click");
     setTimeout(function () {
         $(firstImg).css("order", lastImgOrder + 1);
-        $(".images").css("left", `${moveValue}px`);
+        $(".images").css("margin-left", `${moveValue}px`);
         cellArray.push(cellArray[0]);
         cellArray.shift();
         $(".foward").click(moveFoward);
@@ -29,10 +29,10 @@ function moveBackward() {
     let firstImg = cellArray[0];
     let lastImg = cellArray[5];
     let firstImgOrder = parseInt($(firstImg).css("order"));
-    let moveValue = parseInt($(".images").css("left"));
+    let moveValue = parseInt($(".images").css("margin-left"));
     $(lastImg).css("order", firstImgOrder - 1);
-    $(".images").css("left", `${moveValue - 320}px`)
-    $(".images").animate({ left: `${moveValue}px` }, 900);
+    $(".images").css("margin-left", `${moveValue - 320}px`)
+    $(".images").animate({ marginLeft: `${moveValue}px` }, 900);
     $(".foward").unbind("click");
     $(".backward").unbind("click");
     cellArray.unshift(cellArray[5]);
